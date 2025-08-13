b78709358af4c50a74085481ad75fc3fa0394513
Chris Alcantara <68396707+CAF01@users.noreply.github.com>
2025-08-13 17:04:57 -0600

    Integrate Preline, Tailwind, and new JS plugins
    Added Preline and Tailwind CSS configuration and dependencies, updated Angular build to include Preline scripts, and extended global typings for new plugins (jQuery, DataTable, Dropzone, VanillaCalendarPro, Preline static methods). Removed unused app routes and spec files. Updated styles and HTML to support new integrations.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,37 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { isPlatformBrowser } from '@angular/common';
+import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('stylux-project');
+
+  // constructor(private router:Router, @Inject(PLATFORM_ID) private platformId: Object){
+  //   if(isPlatformBrowser(platformId))
+  //   {
+  //     let theme = localStorage.getItem('hs_theme');
+  //     if (theme) {
+  //       document.documentElement.classList.toggle('dark', theme === 'dark');
+  //     } 
+  //     else 
+  //     {
+  //       document.documentElement.classList.toggle(
+  //         'dark',
+  //         window.matchMedia('(prefers-color-scheme: dark)').matches
+  //       );
+  //     }
+  //   }
+  // }
+
+  // ngOnInit() {
+  //   this.router.events.pipe(
+  //     filter(event => event instanceof NavigationEnd)
+  //   ).subscribe((event: NavigationEnd) => {
+  //   console.log('Navigation ended successfully:', event.urlAfterRedirects);
+  // });
+  // }
 }
